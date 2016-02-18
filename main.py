1957e368f48382e68f243bf6edfb8214d9d911cf
shervin <shervin.h.esmaili@gmail.com>
2016-02-19 01:19:48 +0330

    Added basic input from terminal.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import argparse
+from overhead import __version__
+
+COMMAND_CHOICES = ('show-graph', 'gephi')
+
+
 def main():
+    # TODO: get disc from setup?
+    parser = argparse.ArgumentParser(description="A network scan detector based on analysis of netflow data.")
+    parser.add_argument('-v', '--version', action='version', version='overhead v %s' % __version__,
+                        help="Prints the program's version")
+    parser.add_argument('command', choices=COMMAND_CHOICES, help='The action you want to run.')
+    args = parser.parse_args()
+    if args.command == 'gephi':
+        print "Output for Gephi"
     print "here we go again."
+
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+from overhead import __version__
+
 setup(
-    name = "overhead",
-    version = "0.0.1",
-    description = "A network scan detector based on analysis of netflow data.", 
-    author = "Shervin Hajiesmaili",
-    author_email = "shervin.h.esmaili@gmail.com",
-    py_modules = ['main'],
-    packages = ['overhead'],
-    entry_points = {
+    name="overhead",
+    version=__version__,
+    description="A network scan detector based on analysis of netflow data.",
+    author="Shervin Hajiesmaili",
+    author_email="shervin.h.esmaili@gmail.com",
+    py_modules=['main'],
+    packages=['overhead'],
+    entry_points={
         'console_scripts': ['overhead = main:main']})
