ee5f9130ce49b21aed6983137171a083f739df9a
shervin <shervin.h.esmaili@gmail.com>
2016-05-08 17:11:29 +0430

    Added some basic graph factors.

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import argparse
 from overhead import __version__
 from overhead import graph
 from overhead.analyze import Analyzer
+import json
 
-COMMAND_CHOICES = ('show-graph', 'verticalfactors')
+COMMAND_CHOICES = (
+    'show-graph', 'verticalfactors', 'centrality', 'pagerank', 'clustering-coefficient', 'spectrum',
+    'eccentricity-factors', 'basic-info', 'connected-components', 'degree-distribution')
 
 
 def main():
@@ -18,9 +21,50 @@ def main():
     if args.command == 'show-graph':
         g_builder = graph.GraphBuilder(args.silk_file, args.ip)
         g_builder.show_graph()
+
     if args.command == 'verticalfactors':
-        analyser = Analyzer(args.silk_file, args.ip)
-        analyser.print_factors()
+        analyzer = Analyzer(args.silk_file, args.ip)
+        analyzer.print_factors()
+
+    if args.command == 'centrality':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print 'clossness centrality: ' + json.dumps(g_builder.get_closeness_centrality())
+        print 'betweenness centrality: ' + json.dumps(g_builder.get_betweenness_centrality())
+        print 'eigenvector centrality: ' + json.dumps(g_builder.get_eigenvector_centrality())
+        print 'edge betweenness centrality: ', g_builder.get_edge_betweenness_centrality()
+
+    if args.command == 'pagerank':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print g_builder.get_page_rank()
+
+    if args.command == 'clustering-coefficient':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print g_builder.get_average_clustring_coefficient()
+
+    if args.command == 'spectrum':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print g_builder.get_spectrum()
+
+    if args.command == 'eccentricity-factors':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print 'center: ', g_builder.get_center_node()
+        print 'radius: ', g_builder.get_radius()
+        print 'diameter: ', g_builder.get_diameter()
+
+    if args.command == 'basic-info':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print 'node count:', g_builder.get_node_count()
+        print 'edge count:', g_builder.get_edge_count()
+        print 'density:', g_builder.get_density()
+
+    if args.command == 'connected-components':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        print g_builder.get_connected_component_count()
+
+    if args.command == 'degree-distribution':
+        g_builder = graph.GraphBuilder(args.silk_file, args.ip)
+        g_builder.draw_degree_plot()
+
 
 if __name__ == '__main__':
     main()
--- overhead/graph.py
+++ overhead/graph.py
@@ -1,10 +1,15 @@
+import operator
 import silk
+from matplotlib import pylab
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.linalg.spectrum import adjacency_spectrum
+import numpy as np
 
 
 class GraphBuilder(object):
     graph = None
+    eccentricity = None
 
     def __init__(self, file_name, sip):
         self.sip = silk.IPAddr(sip)
@@ -27,8 +32,120 @@ class GraphBuilder(object):
         pos = dict()
 
         for i in ips:
-                pos[i] = [p for p in ports if self.graph.has_edge(i, p)]
+            pos[i] = [p for p in ports if self.graph.has_edge(i, p)]
         pos.update((n, (1, i)) for i, n in enumerate(ips))
         pos.update((n, (2, i)) for i, n in enumerate(ports))
         nx.draw(self.graph, pos, with_labels=True)
+        plt.show()
+
+    def get_spectrum(self):
+        if self.graph is None:
+            self._create_graph()
+        return adjacency_spectrum(self.graph)
+
+    def get_node_count(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return len(self.graph.nodes())
+
+    def get_edge_count(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return len(self.graph.edges())
+
+    def get_density(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.density(self.graph)
+
+    def get_diameter(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.diameter(self.graph, self._get_eccentricity())
+
+    def get_radius(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.radius(self.graph, self._get_eccentricity())
+
+    def get_center_node(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.center(self.graph, self._get_eccentricity())
+
+    def get_connected_component_count(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.number_connected_components(self.graph)
+
+    def _get_eccentricity(self):
+        if self.graph is None:
+            self._create_graph()
+        if self.eccentricity is None:
+            self.eccentricity = nx.eccentricity(self.graph)
+        return self.eccentricity
+
+    def get_closeness_centrality(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.closeness_centrality(self.graph)
+
+    def get_eigenvector_centrality(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.eigenvector_centrality_numpy(self.graph)
+
+    def get_edge_betweenness_centrality(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.edge_betweenness_centrality(self.graph)
+
+    def get_betweenness_centrality(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.betweenness_centrality(self.graph)
+
+    def get_page_rank(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.pagerank(self.graph)
+
+    def get_average_clustring_coefficient(self):
+        if self.graph is None:
+            self._create_graph()
+
+        return nx.average_clustering(self.graph)
+
+    def draw_degree_plot(self):
+        if self.graph is None:
+            self._create_graph()
+        degree_sequence = sorted(nx.degree(self.graph).items(), key=operator.itemgetter(1), reverse=True)  # degree sequence
+        xs = []
+        ys = []
+        for n in degree_sequence:
+            xs.append(n[0])
+            ys.append(n[1])
+
+        xticks = [str(x) for x in xs]
+        xs = [i for i in range(0, len(xticks))]
+        plt.xticks(xs, xticks)
+        pylab.ylim(ymin=0, ymax=ys[0])
+        plt.plot(xs, ys)
+        plt.title("Degree rank plot")
+        plt.ylabel("degree")
+        plt.xlabel("rank")
+
+        plt.savefig("degree_histogram.png")
         plt.show()
